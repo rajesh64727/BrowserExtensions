5991d7fea10d974822947ac5f5ed1bd57a1d7abc
RajeshKumar <rajesh64727@gmail.com>
2020-09-08 09:26:52 +0530

    LiveMint epaper access added

--- HindustanTimesFreeEpaper/content.js
+++ HindustanTimesFreeEpaper/content.js
@@ -3,15 +3,14 @@ chrome.runtime.onMessage.addListener(processMessage);
 // Useless - Start //
 function processMessage(request, sender, sendResponse) {
     if(request.action == "load_HTEP"){
-        console.log(request); 
+     //   console.log(request); 
     }    
 }
 // Useless - End //
-
 window.onload = function() {
     document.cookie = "_skipSignIn=PKMKB_CKMKB; expires=Sat, 28 Sep 2028 12:00:00 UTC; path=/";
     document.cookie = "token=true; expires=Sat, 28 Sep 2028 12:00:00 UTC; path=/";
     document.cookie = "_allow_action=1; expires=Sat, 28 Sep 2028 12:00:00 UTC; path=/; domain=.livehindustan.com";
-    document.querySelector('header.header').style.display = 'none';
+    document.cookie = "_freegift=1; expires=Sat, 28 Sep 2028 12:00:00 UTC; path=/; domain=epaper.livemint.com";
     console.clear();
 }
