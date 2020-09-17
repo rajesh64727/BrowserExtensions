4cc9ea388d05f20d38abf26c65d71975d9e65ad1
RajeshKumar <rajesh64727@gmail.com>
2020-09-17 23:03:28 +0530

    DJ UI update

--- DJagranChromeExtn/content.js
+++ DJagranChromeExtn/content.js
@@ -1,15 +1,11 @@
-chrome.runtime.onMessage.addListener(
-	function (request, sender, sendResponse) {
-		switch (request.greeting) {
-			case 'clear':				
-				document.cookie = "abc=rajesh64727";				
-				let loginPrompt = document.getElementsByClassName('login-col')[0];
-				if(loginPrompt != null) loginPrompt.parentNode.removeChild(loginPrompt); 
-				document.querySelectorAll('.ads, .gutter-banner, [id*="389882"]').forEach(function(item) {item.remove();})
-				break;
-			default: break;
-		}
-		console.clear();
-		sendResponse({DJResponse: "done"});
-	}
-);
+window.onload = function () {
+  document.cookie = "abc=rajesh64727";
+  let loginPrompt = document.getElementsByClassName("login-col")[0];
+  if (loginPrompt != null) loginPrompt.parentNode.removeChild(loginPrompt);
+  document
+    .querySelectorAll('.ads, .gutter-banner, [id*="389882"]')
+    .forEach(function (item) {
+      item.remove();
+    });
+  console.clear();
+};
