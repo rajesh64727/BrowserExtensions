1c55bc862dab2982b06d0236edf5a9f3b42edd3d
RajeshKumar <rajesh64727@gmail.com>
2021-06-14 06:07:01 +0530

    DJ updates

--- DJagranChromeExtn/content.js
+++ DJagranChromeExtn/content.js
@@ -1,5 +1,12 @@
+const cookieList = ["abc=true",
+"name=test",
+"jg_l=12345",
+"abcuser=a6105c0a611b41b08f1209506350279e", 
+"validauser=a6105c0a611b41b08f1209506350279e",
+"uloc=india",
+"hide=true"];
+
 window.onload = function () {
-  document.cookie = "abc=rajesh64727";
   let loginPrompt = document.getElementsByClassName("login-col")[0];
   if (loginPrompt != null) loginPrompt.parentNode.removeChild(loginPrompt);
   document
@@ -7,5 +14,19 @@ window.onload = function () {
     .forEach(function (item) {
       item.remove();
     });
+    cookieList.forEach(ck => {
+      document.cookie = ck+"; Path=/; Expires=Sat, 01 Jan 2022 00:00:01 GMT; domain=epaper.jagran.com;"
+    })
+
+    if(location.href.indexOf('all-editions') != -1){
+      document.querySelectorAll('.content li > a').forEach( e => {
+        if(e.href.indexOf('_new.html') == -1){
+        e.href = e.href.replace('.html', '_new.html');
+        }
+      });
+    }
+
   console.clear();
 };
+
+
