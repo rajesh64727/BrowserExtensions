ce595c4f6c6d1f69aebba2882893c97e96ef4aac
Rajesh Kumar <rajesh64727@gmail.com>
2025-07-28 20:13:13 +0530

    Update background.js

--- DJagranChromeExtn/background.js
+++ DJagranChromeExtn/background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      files: ['content-script.js']
+      files: ['content.js']
     });
   }
 });
