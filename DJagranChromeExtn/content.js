fcc0faae42e201e7b880e791b99b9c5df0d781ae
Rajesh Kumar <rajeshk@jumpstart.com>
2025-07-29 09:05:31 +0530

    code fix

--- DJagranChromeExtn/content.js
+++ DJagranChromeExtn/content.js
@@ -1,14 +1,15 @@
-const cookieList = ["abc=demouser",
-"name=demouser",
-"jg_l=12345",
-"abcuser=a6105c0a611b41b08f1209506350279e", 
-"validauser=a6105c0a611b41b08f1209506350279e",
-"uloc=india",
-"hide=true"];
-
-
 window.addEventListener('load', () => {
- let loginPrompt = document.getElementsByClassName("login-col")[0];
+
+  const cookieList = ["abc=demouser",
+    "name=demouser",
+    "j_u=demo",
+    "jg_l=12345",
+    "abcuser=a6105c0a611b41b08f1209506350279e", 
+    "validauser=a6105c0a611b41b08f1209506350279e",
+    "uloc=india",
+    "hide=true"];
+
+  let loginPrompt = document.getElementsByClassName("login-col")[0];
   if (loginPrompt != null) loginPrompt.parentNode.removeChild(loginPrompt);
   document
     .querySelectorAll('.ads, .gutter-banner, [id*="389882"]')
@@ -16,7 +17,7 @@ window.addEventListener('load', () => {
       item.remove();
     });
 
-    if(getCookie("abc") == ''){
+    if(getCookie("abcuser") == null || getCookie("abcuser") == ''){
       cookieList.forEach(ck => {        
         document.cookie = ck.split('=')[0] + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/";
         document.cookie = ck+"; Path=/; Expires=Sat, 01 Jan 2029 00:00:01 GMT; domain=epaper.jagran.com;"
@@ -24,12 +25,46 @@ window.addEventListener('load', () => {
      document.location.reload(true);
     }
 
+    function gaevent(newhref, city, ed, epaper){
+      location.href = "https://epaper.jagran.com/epaper/"+newhref;     
+    }
+
     if(location.href.indexOf('all-editions') != -1){
       document.querySelectorAll('.content li > a').forEach( e => {
         if(e.href.indexOf('_new.html') == -1){
         e.href = e.href.replace('.html', '_new.html');
         }
+
+        let onclickString = e.getAttribute('onclick');
+        const parsedParameters = [];
+
+        const functionCallMatch = onclickString.match(/^gaevent\s*\((.*)\)$/);
+
+        if (functionCallMatch && functionCallMatch[1]) {
+          const parametersString = functionCallMatch[1];
+
+          const parameterMatches = parametersString.match(/('[^']*'|[^,]+)/g);
+
+          if (parameterMatches) {
+            parameterMatches.forEach(param => {
+              let cleanedParam = param.trim();
+              
+              if (cleanedParam.startsWith("'") && cleanedParam.endsWith("'")) {
+                cleanedParam = cleanedParam.substring(1, cleanedParam.length - 1);
+              }
+              parsedParameters.push(cleanedParam);
+            });
+          }
+        }
+
+        e.href = "/epaper/"+parsedParameters[0];
       });
+
+
+    }
+
+    function openEpaper(newhref){
+      location.href = "https://epaper.jagran.com/epaper/"+newhref;     
     }
 
   console.clear();
